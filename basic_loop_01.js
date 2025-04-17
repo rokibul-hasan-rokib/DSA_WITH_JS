b45e9be69fb6f703deffc872465d9e5c34895a41
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-17 20:44:50 +0600

    loop basic code done

---
+++ basic_loop_01.js
@@ -0,0 +1,43 @@
+//normal loop
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//while loop
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+//do while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+
+//for in loop
+const array = [1, 2, 3, 4, 5];
+for (const index in array) {
+    console.log(index);
+}
+
+//for of loop
+const array1 = [1, 2, 3, 4, 5];
+for (const value of array1) {
+    console.log(value);
+}
+
+//for each loop
+const array2 = [1, 2, 3, 4, 5];
+array2.forEach((value) => {
+    console.log(value);
+});
+
+//forEach loop
+const array3 = [1, 2, 3, 4, 5];
+array3.forEach((value) => {
+    console.log(value);
+});
+
