42dd55fa177e8968fed27e209babb4f75d4b7249
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-21 23:57:10 +0600

    binary search demo

---
+++ binary_search_demo.js
@@ -0,0 +1,11 @@
+function binarySearch(arr, target) {
+    let low = 0, high = arr.length - 1;
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (arr[mid] === target) return mid;
+      else if (arr[mid] < target) low = mid + 1;
+      else high = mid - 1;
+    }
+    return -1;
+  }
+  
